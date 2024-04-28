288a22e1120fefeedb94fb72856736b012be9fe8
Ryo Ishibashi <bridgestone.amatsu0228@gmail.com>
2024-04-29 04:40:27 +0900

    目次用のh1~h3タグのid取得を実装した

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -15,6 +15,8 @@ import {
   Title,
   Space,
   Divider,
+  AppShell,
+  Affix,
 } from "@mantine/core";
 import { client } from "libs/client";
 import Link from "next/link";
@@ -22,50 +24,71 @@ import parse from "html-react-parser";
 import { createClient } from "microcms-js-sdk";
 import { motion } from "framer-motion";
 import { TweetBlogButton } from "@/features/HomePage/blog/_components/TweetBlogButton";
+import * as cheerio from "cheerio";
 
 export default function BlogId({ blog }: { blog: any }) {
   // console.log(blog);
   const matches = useMediaQuery("(min-width: 768px)");
+  const showToc = useMediaQuery("(max-width: 1650px)");
+
+  // console.log(showToc);
 
   const publishedDate = new Date(blog.publishedAt);
   const updatedDate = new Date(blog.updatedAt);
   const formattedPublishedDate = `${publishedDate.getFullYear()}/${publishedDate.getMonth() + 1}/${publishedDate.getDate()}`;
   const formattedUpdatedDate = `${updatedDate.getFullYear()}/${updatedDate.getMonth() + 1}/${updatedDate.getDate()}`;
 
+  const $ = cheerio.load(blog.content);
+  const headings = $("h1, h2, h3").toArray();
+  const toc = headings.map((data) => {
+    if (data.children[0].type === "text") {
+      return {
+        text: data.children[0].data,
+        id: data.attribs.id,
+        name: data.name,
+      };
+    }
+  });
+
+  console.log(toc);
+
   return (
     <Stack align="stretch" justify="space-between" mih="100vh">
       <Box>
-        <Header />{" "}
+        <Header />
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.75 }}
         >
-          <Box
-            maw={800}
-            p={"10px 20px"}
-            m={"0px auto 20px auto"}
-            c="rgb(32, 16, 12)"
-          >
-            <Center>
-              <Title order={1} mt={40} size={28}>
-                {blog.title}
-              </Title>
-            </Center>
-            <Space h={20} />
-            <Group justify="space-between">
-              <Group>
-                <Text>投稿日：{formattedPublishedDate}</Text>
-                <Text>更新日：{formattedPublishedDate}</Text>
+          <AppShell>
+            <Box
+              maw={800}
+              p={"10px 20px"}
+              m={"0px auto 20px auto"}
+              c="rgb(32, 16, 12)"
+            >
+              <Center>
+                <Title order={1} mt={40} size={28}>
+                  {blog.title}
+                </Title>
+              </Center>
+              <Space h={20} />
+              <Group justify="space-between">
+                <Group>
+                  <Text>投稿日：{formattedPublishedDate}</Text>
+                  <Text>更新日：{formattedPublishedDate}</Text>
+                </Group>
+                <Text>カテゴリ： {blog.category?.name ?? "ないよ(笑)"}</Text>
               </Group>
-              <Text>カテゴリ： {blog.category?.name ?? "ないよ(笑)"}</Text>
-            </Group>
-            <Divider mt={20} size="md" color="" />
-            <Space h={20} />
-            <Box className="blogContent">{parse(blog.content)}</Box>
-            <TweetBlogButton title={blog.title} />
-          </Box>
+              {/* <Divider mt={20} size="md" /> */}
+              <Space h={20} />
+
+              <Box className="blogContent">{parse(blog.content)}</Box>
+              <TweetBlogButton title={blog.title} />
+            </Box>
+          </AppShell>
         </motion.div>
       </Box>
       <Footer />
